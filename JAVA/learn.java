4acb6e5e70c94257fbc334e4cf6bb9d6faf16f62
Pradnyesh <kakulatepradnyesh09@gmail.com>
2026-08-03 21:30:22 +0530

    Java: switch statement example

---
+++ JAVA/learn.java
@@ -0,0 +1,107 @@
+// public class learn {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+
+// public class learn{
+//     public static void main(String[] args) {
+//         System.out.println("*");
+//         System.out.println("**");
+//         System.out.println("***");
+//         System.out.println("****");
+//         System.out.println("*****");
+//     }
+// }
+
+
+// public class learn{
+//     public static void main(String[] args) {
+//      // variables
+//      String name = "John";
+//         double price = 10.99;
+//         int age = 21;
+//         int a = 25;
+//         int b = 10;
+
+
+//     }
+// }
+
+// public class learn {
+//     public static void main(String[] args){
+//         // Variables
+//         int a = 26;
+//         int b = 10;
+//         int sum = a + b;
+//         //System.out.println(sum);
+//         int diff = a - b;
+//         //System.out.println(diff);
+//         int mul = a * b;
+//         //System.out.println(mul);
+//         int div = a / b;
+//         //System.out.println(div);
+//         int mod = a % b;
+//         // System.out.println(mod);  
+//     }
+// }
+
+
+// import java.util.Scanner;
+
+// public class learn {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int a = sc.nextInt();  // Reads first integer
+//         int b = sc.nextInt();  // Reads second integer
+
+//         int sum = a + b;
+//         System.out.println(sum);
+//     }
+// }
+
+// import java.util.*;
+
+// public class learn {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int age = sc.nextInt();
+
+//         if (age >= 18) {
+//             System.out.println("Adult");
+//         } else {
+//             System.out.println("Not Adult");
+//         }
+
+//         sc.close();
+//     }
+// }
+
+import java.util.*;
+
+public class learn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        if (x % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+}
+
+
+
+// Switch statement
+// class LearnSwitch {
+//     public static void main(String[] args) {
+//         int day = 3;
+//         switch(day) { case 1: System.out.println("Mon"); break; default: System.out.println("Other"); }
+//     }
+// }
