82a73f82dd6e72ef1451adbffc26d4bcca385e07
Pradnyesh <kakulatepradnyesh09@gmail.com>
2026-08-03 21:30:22 +0530

    Java: do while loop example

--- JAVA/learn.java
+++ JAVA/learn.java
@@ -120,3 +120,11 @@ public class learn {
 //         while(i<5) { System.out.println(i); i++; }
 //     }
 // }
+
+// Do While Loop
+// class LearnDoWhile {
+//     public static void main(String[] args) {
+//         int i=0;
+//         do { System.out.println(i); i++; } while(i<5);
+//     }
+// }
