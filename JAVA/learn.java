a47c035dd7f141c0f45efa9ee4d395cab978bb48
Pradnyesh <kakulatepradnyesh09@gmail.com>
2026-08-03 21:30:22 +0530

    Java: continue statement example

--- JAVA/learn.java
+++ JAVA/learn.java
@@ -135,3 +135,10 @@ public class learn {
 //         for(int i=0; i<10; i++) { if(i==5) break; System.out.println(i); }
 //     }
 // }
+
+// Continue Statement
+// class LearnContinue {
+//     public static void main(String[] args) {
+//         for(int i=0; i<10; i++) { if(i%2==0) continue; System.out.println(i); }
+//     }
+// }
